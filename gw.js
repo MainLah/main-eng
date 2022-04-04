4991a15ab6498cbbad5cc5ea031b81e0d80e3add
MainLah <rendrakusumab@gmail.com>
2022-04-04 20:22:56 +0700

    refactor

--- gw.js
+++ gw.js
@@ -7,39 +7,53 @@ const score = document.getElementById("score");
 const hint = document.getElementById("hint");
 let globalRightAnswer = "";
 
-getQuoteBtn.addEventListener("click", async function getQuote() {
-  await fetch("https://hargrimm-wikihow-v1.p.rapidapi.com/steps?count=1", {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "hargrimm-wikihow-v1.p.rapidapi.com",
-      "x-rapidapi-key": "API_KEY",
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => {
-      score.innerHTML = "";
-      const question = json[1];
-      const array = question
-        .split(" ")
-        .filter(
-          (e) => e.length > 2 && e.length < 9 && !e.includes(`${/[1-9]/g}`)
-        );
-      const rightAnswer = array[
-        Math.floor(Math.random() * array.length)
-      ].replace(/[.;,]/g, "");
-      globalRightAnswer = rightAnswer;
-      currentQuestion.innerText = question.replace(
-        rightAnswer,
-        "_".repeat(rightAnswer.length)
-      );
-      hint.innerText = `Hint: The word is ${
-        rightAnswer.length
-      } letters long and it starts with '${rightAnswer[0].toUpperCase()}'.`;
-      seeAnswer.innerText = "";
-      score.className = "";
-      document.getElementById("user-answer").value = "";
-    });
+getQuoteBtn.addEventListener("click", getQuote);
+
+submitBtn.addEventListener("click", checkAnswer);
+
+checkAnswerBtn.addEventListener("click", () => {
+  seeAnswer.innerText = globalRightAnswer;
 });
+
+function getQuote() {
+  try {
+    fetch("https://hargrimm-wikihow-v1.p.rapidapi.com/steps?count=1", {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "hargrimm-wikihow-v1.p.rapidapi.com",
+        "x-rapidapi-key": "API_KEY",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => appendQuestion(json))
+      .catch((err) => console.error(err));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function appendQuestion(json) {
+  score.innerHTML = "";
+  const question = json[1];
+  const array = question
+    .split(" ")
+    .filter((e) => e.length > 2 && e.length < 9 && !e.includes(`${/[1-9]/g}`));
+  const rightAnswer = array[Math.floor(Math.random() * array.length)].replace(
+    /[.;,]/g,
+    ""
+  );
+  globalRightAnswer = rightAnswer;
+  currentQuestion.innerText = question.replace(
+    rightAnswer,
+    "_".repeat(rightAnswer.length)
+  );
+  hint.innerText = `Hint: The word is ${
+    rightAnswer.length
+  } letters long and it starts with '${rightAnswer[0].toUpperCase()}'.`;
+  seeAnswer.innerText = "";
+  score.className = "";
+  document.getElementById("user-answer").value = "";
+}
 
 function checkAnswer() {
   const userAnswer = document.getElementById("user-answer").value;
@@ -55,12 +69,6 @@ function checkAnswer() {
     }, 350);
   }
 }
-
-checkAnswerBtn.addEventListener("click", () => {
-  seeAnswer.innerText = globalRightAnswer;
-});
-
-submitBtn.addEventListener("click", checkAnswer);
 
 function checkKey(e) {
   if (e.keyCode == 13) {
